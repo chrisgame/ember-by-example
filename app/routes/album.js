0ec0dc5b38bbb9d4f9ef5d92d1ac60bd915e6b3a
The Game <chris.game@robbiebobbins.com>
2015-04-07 19:46:33 +0100

    moved check for out of range album id to before model hook

--- app/routes/album.js
+++ app/routes/album.js
@@ -1,10 +1,13 @@
 import Em from 'ember';
 
 export default Em.Route.extend({
-  model: function(params) {
-    if(params.album_id > 7) {
-      this.transitionTo('artists');
+  beforeModel: function(transition) {
+    if(transition.state.params.album.album_id > 7) {
+      this.transitionTo('artist');
     }
+  },
+
+  model: function(params) {
     return this.store.find('album', params.album_id);
   }
 });
